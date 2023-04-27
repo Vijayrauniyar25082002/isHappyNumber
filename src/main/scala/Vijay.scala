0169edc481ef72ea2f76756b9b684582786c63e9
Vijay <vijaykumar983832@gmail.com>
2023-04-27 17:50:37 +0530

    Initial Version

---
+++ src/main/scala/Vijay.scala
@@ -0,0 +1,36 @@
+import scala.io.StdIn._
+object Vijay {
+
+  def main (args:Array[String]):Unit = {
+
+    var num = readInt
+    var number = num
+
+    while(number!=1 && number!=4)
+      {
+        number = isHappy(number)
+      }
+      if (number==1)
+        {
+          println(number +" is Happy Number")
+        }
+      else
+        {
+          println(number+" is not Happy Number")
+        }
+  }
+
+  def isHappy(n: Int):Int = {
+    var numb = n
+    var res = 0
+
+    while (numb>0)
+      {
+        var rem = numb%10
+            res = res+(rem*rem)
+            numb = numb/10
+      }
+      res
+  }
+
+}
